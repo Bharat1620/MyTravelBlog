697e47e8149a1143ab3fcab3db0d08727c4cd7e4
Bharat Agrawal <18ucc018@lnmiit.ac.in>
2021-12-10 17:15:08 +0530

    updated app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/travel",
+    callbackURL: "https://mytravelsites.herokuapp.com/auth/google/travel",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
